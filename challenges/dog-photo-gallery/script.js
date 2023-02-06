f938fa75344075830d72c4d2a37d4f523cdbebbf
MacOS <macos@blessing.home>
2023-02-06 22:48:39 +0000

    feat: finishing 1 challenge

--- challenges/dog-photo-gallery/script.js
+++ challenges/dog-photo-gallery/script.js
@@ -1,24 +1,20 @@
-// async function getRandomDog() {
-//     const request = await fetch("https://dog.ceo/api/breeds/image/random");
-//     const blob = await request.blob();
-//     const objectURL = URL.createObjectURL(blob);
-//     console.log(objectURL)
-//     return objectURL;
-// }
-
-async function getRandomDog() {
-fetch('https://dog.ceo/api/breeds/image/random')
-  .then((response) => response.json())
-  .then((data) => data.message)
-  .then((error) =>console.log(error))
-}
 
 const button = document.getElementById("random-dog");
 const dogContainer = document.getElementById("dog-container");
 
-button.addEventListener('click', async () => {
-    const img = document.createElement('img');
-    img.src = await getRandomDog();
-    dogContainer.innerHTML = '';
-    dogContainer.appendChild(img);
+button.addEventListener('click', getRandomDog)
+
+function getRandomDog() {
+fetch("https://dog.ceo/api/breeds/image/random")
+.then(response => {
+  return response.json();
 })
+.then(data => {
+  const img = document.createElement('img');
+  img.src = data.message;
+  dogContainer.innerHTML = '';
+  dogContainer.appendChild(img);
+  
+})
+.catch((error) => console.log(error));
+}
